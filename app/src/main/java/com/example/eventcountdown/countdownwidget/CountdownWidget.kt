b1911babfed284474b3ba5e2f344f40c53ec41de
Tony Spegel <TonySpegel@googlemail.com>
2024-03-20 00:52:43 +0100

    add clickable to open an event in google calendar and improve styling

--- app/src/main/java/com/example/eventcountdown/countdownwidget/CountdownWidget.kt
+++ app/src/main/java/com/example/eventcountdown/countdownwidget/CountdownWidget.kt
@@ -7,11 +7,14 @@ import androidx.compose.ui.unit.sp
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
 import androidx.glance.GlanceTheme
+import androidx.glance.action.clickable
 import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.cornerRadius
 import androidx.glance.appwidget.lazy.LazyColumn
 import androidx.glance.appwidget.provideContent
 import androidx.glance.background
 import androidx.glance.layout.Alignment
+import androidx.glance.layout.Box
 import androidx.glance.layout.Column
 import androidx.glance.layout.Row
 import androidx.glance.layout.fillMaxSize
@@ -24,6 +27,7 @@ import com.example.eventcountdown.helper.CalendarEvent
 import com.example.eventcountdown.helper.cLTT
 import com.example.eventcountdown.helper.calendarDisplayName
 import com.example.eventcountdown.helper.getRemainingDays
+import com.example.eventcountdown.helper.openCalendarEvent
 import com.example.eventcountdown.helper.readCalendarEvents
 
 class CountdownWidget : GlanceAppWidget() {
@@ -47,23 +51,28 @@ private fun WidgetContentView(ctx: Context) {
     Column(
         modifier = GlanceModifier
             .fillMaxSize()
-            .background(GlanceTheme.colors.primaryContainer)
-            .padding(16.dp),
-        verticalAlignment = Alignment.Top,
+            .background(GlanceTheme.colors.surface)
+            .padding(start = 16.dp, end = 16.dp, top = 16.dp, bottom = 0.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(
-            text = "$displayName Countdown",
-            modifier = GlanceModifier.padding(horizontal = 18.dp),
-            style = TextStyle(
-                fontSize = 20.sp,
-                color = GlanceTheme.colors.onPrimaryContainer
+        Row {
+            Text(
+                text = "$displayName Countdown",
+                modifier = GlanceModifier.padding(start = 18.dp, end = 18.dp, bottom = 8.dp),
+                style = TextStyle(
+                    fontSize = 20.sp,
+                    color = GlanceTheme.colors.onSurface
+                )
             )
-        )
-        LazyColumn {
-            events.forEach { event ->
+        }
+
+        LazyColumn(
+            horizontalAlignment = Alignment.Start,
+            modifier = GlanceModifier.fillMaxWidth()
+        ) {
+            events.forEach {
                 item {
-                    WidgetEventItem(event)
+                    WidgetEventItem(it, ctx)
                 }
             }
         }
@@ -71,43 +80,40 @@ private fun WidgetContentView(ctx: Context) {
 }
 
 @Composable
-private fun WidgetEventItem(event: CalendarEvent) {
-    val (_, eventTitle, startTime, endTime) = event
+private fun WidgetEventItem(event: CalendarEvent, ctx: Context) {
+    val (eventId, eventTitle, startTime, _) = event
 
     val start = cLTT(startTime)
     val remainingDays = getRemainingDays(startTime)
 
-    Row(
-        modifier = GlanceModifier
-            .fillMaxWidth()
-            .padding(
-                horizontal = 16.dp,
-                vertical = 8.dp,
-            )
-    ) {
-        Column {
-            Text(
-                text = eventTitle,
-                style = TextStyle(
-                    fontSize = 16.sp,
-                    fontWeight = FontWeight.Medium,
-                    color = GlanceTheme.colors.onPrimaryContainer
-                )
-            )
-            Text(
-                text = start,
-                style = TextStyle(color = GlanceTheme.colors.onPrimaryContainer)
-            )
-        }
-
+    Box(modifier = GlanceModifier.padding(vertical = 6.dp)) {
         Column(
-            modifier = GlanceModifier.defaultWeight()
+            modifier = GlanceModifier
+                .padding(
+                    horizontal = 16.dp,
+                    vertical = 8.dp,
+                )
+                .fillMaxWidth()
+                .background(GlanceTheme.colors.tertiaryContainer)
+                .cornerRadius(16.dp)
+                .clickable { openCalendarEvent(ctx, eventId) }
         ) {
-            Text(
-                text = "$remainingDays Tage",
-                modifier = GlanceModifier.fillMaxWidth(),
-                style = TextStyle(color = GlanceTheme.colors.onPrimaryContainer)
-            )
+            Row {
+                Text(
+                    text = eventTitle,
+                    style = TextStyle(
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Medium,
+                        color = GlanceTheme.colors.onTertiaryContainer
+                    ),
+                )
+            }
+            Row {
+                Text(
+                    text = "$start · $remainingDays Tage",
+                    style = TextStyle(color = GlanceTheme.colors.onTertiaryContainer)
+                )
+            }
         }
     }
 }
