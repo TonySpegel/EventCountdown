1acb6bfa184e6ed0901ba0a07dcffc18c2264787
Tony Spegel <TonySpegel@googlemail.com>
2024-03-19 02:11:12 +0100

    add functions: CalendarNotGranted|Denied

--- app/src/main/java/com/example/eventcountdown/ui/views/CalendarList.kt
+++ app/src/main/java/com/example/eventcountdown/ui/views/CalendarList.kt
@@ -1,4 +1,23 @@
 package com.example.eventcountdown.ui.views
 
-class CalendarList {
+import android.Manifest
+import androidx.activity.result.ActivityResultLauncher
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CalendarNotGranted(requestPermissionLauncher: ActivityResultLauncher<String>) {
+    Button(
+        onClick = {
+            requestPermissionLauncher.launch(Manifest.permission.READ_CALENDAR)
+        }
+    ) {
+        Text(text = "Berechtigung erteilen")
+    }
+}
+
+@Composable
+fun CalendarDenied() {
+    Text(text = "Berechtigung wurde abgelehnt. Die App benötigt diese Berechtigung, um die Kalender anzuzeigen.")
 }
