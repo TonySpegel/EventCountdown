a17f26e194d5806b26a3b1ffcb1846cd3483f98c
Tony Spegel <TonySpegel@googlemail.com>
2024-03-25 01:51:28 +0100

    use calendar id for widget

--- app/src/main/java/com/example/eventcountdown/countdownwidget/CountdownWidget.kt
+++ app/src/main/java/com/example/eventcountdown/countdownwidget/CountdownWidget.kt
@@ -4,47 +4,57 @@ import android.content.Context
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.intPreferencesKey
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
 import androidx.glance.GlanceTheme
+import androidx.glance.LocalContext
 import androidx.glance.action.clickable
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.cornerRadius
 import androidx.glance.appwidget.lazy.LazyColumn
 import androidx.glance.appwidget.provideContent
 import androidx.glance.background
+import androidx.glance.currentState
 import androidx.glance.layout.Alignment
 import androidx.glance.layout.Box
 import androidx.glance.layout.Column
 import androidx.glance.layout.Row
+import androidx.glance.layout.fillMaxHeight
 import androidx.glance.layout.fillMaxSize
 import androidx.glance.layout.fillMaxWidth
 import androidx.glance.layout.padding
 import androidx.glance.text.FontWeight
 import androidx.glance.text.Text
 import androidx.glance.text.TextStyle
+import com.example.eventcountdown.R
 import com.example.eventcountdown.helper.CalendarEvent
 import com.example.eventcountdown.helper.cLTT
 import com.example.eventcountdown.helper.calendarDisplayName
-import com.example.eventcountdown.helper.getRemainingDays
+import com.example.eventcountdown.helper.getRelativeTimeString
 import com.example.eventcountdown.helper.openCalendarEvent
 import com.example.eventcountdown.helper.readCalendarEvents
+import java.time.Duration
+import java.time.Instant
 
 class CountdownWidget : GlanceAppWidget() {
     override suspend fun provideGlance(context: Context, id: GlanceId) {
         provideContent {
+            val prefs = currentState<Preferences>()
+            val calendarId = prefs[intPreferencesKey("calendarId")] ?: 0
+
             GlanceTheme(
                 GlanceTheme.colors
             ) {
-                WidgetContentView(context)
+                WidgetContentView(context, calendarId)
             }
         }
     }
 }
 
 @Composable
-private fun WidgetContentView(ctx: Context) {
-    val calendarId = 18
+private fun WidgetContentView(ctx: Context, calendarId: Int) {
     val events = readCalendarEvents(ctx, calendarId)
     val displayName = calendarDisplayName(ctx, calendarId)
 
@@ -57,7 +67,7 @@ private fun WidgetContentView(ctx: Context) {
     ) {
         Row {
             Text(
-                text = "$displayName Countdown",
+                text = "$displayName · Countdown",
                 modifier = GlanceModifier.padding(start = 18.dp, end = 18.dp, bottom = 8.dp),
                 style = TextStyle(
                     fontSize = 20.sp,
@@ -66,15 +76,41 @@ private fun WidgetContentView(ctx: Context) {
             )
         }
 
-        LazyColumn(
-            horizontalAlignment = Alignment.Start,
-            modifier = GlanceModifier.fillMaxWidth()
+        when {
+            events.isEmpty() -> WidgetNoEvents()
+            else -> WidgetEvents(events, ctx)
+        }
+    }
+}
+
+@Composable
+private fun WidgetNoEvents() {
+    Column {
+        Box(
+            modifier = GlanceModifier
+                .fillMaxWidth()
+                .fillMaxHeight(),
+            contentAlignment = Alignment.Center
         ) {
-            events.forEach {
-                item {
-                    WidgetEventItem(it, ctx)
-                }
-            }
+            Text(
+                text = LocalContext.current.getString(R.string.no_events),
+                style = TextStyle(
+                    fontSize = 18.sp,
+                    color = GlanceTheme.colors.onSurface
+                )
+            )
+        }
+    }
+}
+
+@Composable
+private fun WidgetEvents(events: List<CalendarEvent>, ctx: Context) {
+    LazyColumn(
+        horizontalAlignment = Alignment.Start,
+        modifier = GlanceModifier.fillMaxWidth()
+    ) {
+        events.forEach {
+            item { WidgetEventItem(it, ctx) }
         }
     }
 }
@@ -82,9 +118,13 @@ private fun WidgetContentView(ctx: Context) {
 @Composable
 private fun WidgetEventItem(event: CalendarEvent, ctx: Context) {
     val (eventId, eventTitle, startTime, _) = event
-
     val start = cLTT(startTime)
-    val remainingDays = getRemainingDays(startTime)
+    val duration = Duration.between(
+        Instant.now(),
+        Instant.ofEpochMilli(startTime)
+    )
+
+    val relativeDuration = getRelativeTimeString(duration).toString()
 
     Box(modifier = GlanceModifier.padding(vertical = 6.dp)) {
         Column(
@@ -110,7 +150,7 @@ private fun WidgetEventItem(event: CalendarEvent, ctx: Context) {
             }
             Row {
                 Text(
-                    text = "$start · $remainingDays Tage",
+                    text = "$start · $relativeDuration",
                     style = TextStyle(color = GlanceTheme.colors.onTertiaryContainer)
                 )
             }
